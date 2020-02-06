a61187ef3f14a1c2f57153b0e760777d63e9e6de
f.santos <frederic.santos@u-bordeaux.fr>
2020-02-06 18:05:33 +0100

    Update chapter 6

--- chapter6_intro.el
+++ chapter6_intro.el
@@ -1,6 +1,31 @@
-;;;;;;;;;;;;;;
-;;; CHAPITRE 6
+;;; chapter6_intro.el --- Solutions to the exercises of chapter 6
+;;; from "An Introduction to Programming in Emacs Lisp",
+;;; by R. Chassell.
 
+;;; Author: Frédéric Santos (2020)
+;;; URL: https://gitlab.com/f.santos/answers-chassell-lisp
+
+;;;;;;;;;;;;;;;;;;;;
+;;; § 6.3. Exercise.
+;; Write a function that will display the first 60 characters of the
+;; current buffer, even if you have narrowed the buffer to its latter half
+;; so that the first line is inaccessible.
+;; Restore point, mark, and narrowing.
+(defun first-sixty-chars ()
+  "Display the first 60 characters of the current buffer (even if)
+they are in a narrowed part."
+  (interactive)
+  (save-restriction
+    (widen)
+    (message "%s"
+	     (buffer-substring-no-properties 1 61))))
+
+;; Test this function:
+(first-sixty-chars)
+
+;;; The next section (in french) is for my own reference only.
+
+;;;;;;;;;;;;;;;;;;;;
 ;;; § 6.2. what-line
 ;; Dans un premier temps on pourrait se contenter de ceci :
 (defun simple-what-line ()
@@ -15,8 +40,7 @@ the current line number of point in the (widened) buffer."
     (widen)
     ;; Get line number in widened buffer:
     (message "Line %d"
-	     (+ 1 (count-lines 1 (point))))
-    ))
+	     (+ 1 (count-lines 1 (point))))))
 
 ;; Mais on constate que la fonction ne marche pas bien.
 ;; L'exécuter avec le point au début d'une ligne, puis au milieu de la
@@ -40,19 +64,5 @@ the current line number of point in the (widened) buffer."
       (beginning-of-line)
       ;; Get line number in widened buffer:
       (message "Line %d"
-	       (+ 1 (count-lines 1 (point))))
-      )))
+	       (+ 1 (count-lines 1 (point)))))))
 
-;;; § 6.3. Exercice.
-(defun first-sixty-chars ()
-  "Display the first 60 characters of the current buffer (even if)
-they are in a narrowed part."
-  (interactive)
-  (save-restriction
-    (widen)
-    (message "%s"
-	     (buffer-substring-no-properties 1 61)
-    )
-    ))
-
-(first-sixty-chars)
