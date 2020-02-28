6f2e7e702729bee1252ec8b7a06e67cd636c2254
f.santos <frederic.santos@u-bordeaux.fr>
2020-02-28 10:29:29 +0100

    End of chapter 12

--- chapter12_intro.el
+++ chapter12_intro.el
@@ -5,6 +5,37 @@
 ;;; Author: Frédéric Santos (2020)
 ;;; URL: https://gitlab.com/f.santos/answers-chassell-lisp
 
+;;;;;;;;;;;;;;;;;;;;;
+;;; § 12.6. Exercises
+;;; (i) Write a function to search for a regular expression that
+;;; matches two or more blank lines in sequence.
+(defun search-blank-lines ()
+  "Search for two or more blank lines in sequence."
+  (interactive)
+  (re-search-forward "\\(^\n\\)\\{2,\\}"))
+
+;;; Begin test of the function
+
+
+
+;;; End test of the function
+;;; Commentary: I'm not sure of this one.
+;;; Note that "in a Lisp program, each '\' must be doubled".
+;;; (See GNU Emacs manual, § 12.6, p. 104)
+
+;;; (ii) Write a function to search for duplicated words.
+(defun dup-words ()
+  "Simplified research for duplicated words."
+  (interactive)
+  (push-mark)				; so as to come back easily
+  (if (re-search-forward "\\(\\w+[\t\n ]+\\)\\1")
+      (message "This is a duplicate.")
+    (message "No duplicates found in this buffer.")))
+
+;;; Test of duplicated duplicated words.
+
+;;; The following sections are given for my own reference.
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; § 12.3. forward-sentence
 (defun perso-forward-sentence (&optional arg)
